89e22606ca868b4e2736269d9b5bcf3837a61f2e
sk7746-ad <sk7746@srmist.edu.in>
2026-09-12 10:43:27 +0530

    Add Week 4 Array assignment problems

---
+++ src/main/java/array/assigment_problems/SubarraySumEqualsK.java
@@ -0,0 +1,27 @@
+package array.assigment_problems;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> prefixSum = new HashMap<>();
+
+        prefixSum.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (prefixSum.containsKey(sum - k)) {
+                count += prefixSum.get(sum - k);
+            }
+
+            prefixSum.put(sum, prefixSum.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
---
+++ src/main/java/array/assigment_problems/ThreeSum.java
@@ -0,0 +1,52 @@
+package array.assigment_problems;
+
+import java.util.Arrays;
+
+public class ThreeSum {
+
+    public static int[][] threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int[][] result = new int[100][3];
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result[count][0] = nums[i];
+                    result[count][1] = nums[left];
+                    result[count][2] = nums[right];
+                    count++;
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return Arrays.copyOf(result, count);
+    }
+}
